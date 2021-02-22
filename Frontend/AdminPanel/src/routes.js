f623752bdb182fc707421364136621e87bc6ec87
Pratik Maniya <pratik.maniya@tristatetechnology.co.in>
2021-02-22 13:43:55 +0530

    added users page and added searching in taxis page

--- Frontend/AdminPanel/src/routes.js
+++ Frontend/AdminPanel/src/routes.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const Taxis = React.lazy(() => import('./views/Taxis/Taxis'));
+const TaxiDetails = React.lazy(() => import('./views/TaxiDetails/TaxiDetails'));
 const Users = React.lazy(() => import('./views/Users/Users'));
 
 const routes = [
   { path: '/taxis', exact: true, name: 'Taxis', component: Taxis },
+  { path: '/taxi-details/:taxi_id', exact: true, name: 'Taxis', component: TaxiDetails },
   { path: '/users', exact: true, name: 'Users', component: Users },
 ];
 
--- Frontend/AdminPanel/src/views/Users/Users.js
+++ Frontend/AdminPanel/src/views/Users/Users.js
@@ -1,76 +1,42 @@
 import React, { Component } from 'react';
-import { Card, CardBody, CardHeader, Col, Row, Table, Button, Input } from 'reactstrap';
+import { Card, CardBody, CardHeader, Col, Row, Table } from 'reactstrap';
 import Pagination from "react-js-pagination";
-import Img from 'react-image';
 import { connect } from 'react-redux';
 
-import { apiCall, displayLog } from '../../utils/common';
+import { apiCall } from '../../utils/common';
 import store from '../../utils/store';
 import * as actionTypes from '../../store/actionTypes';
-import loading_image from '../../assets/images/loading_img.png'
-import default_img from '../../assets/images/default_img.png'
 import config from '../../config';
 
 class Users extends Component {
     state = {
         users: [],
         totalItemCount: 0,
-        search_keyword: "",
         page_no: this.props.global_page_no
     }
-    getUsers = async (reqData) => {
-        let response = await apiCall('POST', 'users', reqData);
+    getUsers = async (data) => {
+        let response = await apiCall('GET', `users?page_no=${data.page_no}`);
         this.setState({ users: response.data.users, totalItemCount: response.data.total_users });
     }
     componentDidMount() {
         let data = {
             page_no: this.props.global_page_no
         }
-        if (this.props.global_search_keyword) {
-            data.query_string = this.props.global_search_keyword
-        }
         this.getUsers(data)
         this.setState({
-            page_no: this.props.global_page_no,
-            search_keyword: this.props.global_search_keyword
+            page_no: this.props.global_page_no
         })
     }
     async componentDidUpdate(prevProps) {
-        if (prevProps.global_page_no !== this.props.global_page_no
-            || prevProps.global_search_keyword !== this.props.global_search_keyword) {
+        if (prevProps.global_page_no !== this.props.global_page_no) {
             let data = {
                 page_no: this.props.global_page_no
             }
-            if (this.props.global_search_keyword) {
-                data.query_string = this.props.global_search_keyword
-            }
             await this.getUsers(data);
             this.setState({
-                search_keyword: this.props.global_search_keyword,
                 page_no: this.props.global_page_no
             })
 
-        }
-    }
-    changeSearch = (event) => {
-        this.setState({ [event.target.name]: event.target.value })
-    }
-    enterPressed = (event) => {
-        if (event.keyCode === 13 || event.which === 13) {
-            this.search()
-        }
-    }
-    search() {
-        if ((this.state.search_keyword.length === 0 && this.state.search_keyword.trim().length === 0) || (this.state.search_keyword.trim().length > 0)) {
-            let query_string = this.state.search_keyword.trim().length === 0 ? undefined : this.state.search_keyword.trim()
-            store.dispatch({
-                type: actionTypes.SET_PAGE_NO,
-                page_no: 1
-            })
-            store.dispatch({
-                type: actionTypes.SET_SEARCH_KEYWORD,
-                search_keyword: query_string ? query_string : ""
-            })
         }
     }
     handlePageChange = pageNumber => {
@@ -83,26 +49,10 @@ class Users extends Component {
         return (
             <tr key={index}>
                 <td className="text-center">{(this.state.page_no - 1) * config.LIMIT + index + 1}</td>
-                <td className="align-middle">
-                    <Img
-                        className="table-cell-img"
-                        src={user.profile_image}
-                        loader={<img className="table-cell-img loading-img" alt="motologs" src={loading_image} />}
-                        unloader={<img className="table-cell-img" alt="motologs" title="No Image Found" src={default_img} />}
-                    />
-                </td>
-                <td className="align-middle">{user.name ? user.name : "N/A"}</td>
-                <td className="align-middle text-left">{user.email}</td>
-                <td className="align-middle">{user.address ? user.address : "N/A"}</td>
-                <td className="align-middle">
-                    {
-                        user.is_active
-                            ?
-                            <span className={"fa fa-toggle-off active action-icon"} title={"Deactivate User"} onClick={() => this.activeClickHandler(user, false, index)}  ></span>
-                            :
-                            <span className={"fa fa-toggle-on active action-icon"} title={"Activate User"} onClick={() => this.activeClickHandler(user, true, index)}  ></span>
-                    }
-                </td>
+                <td className="align-middle">{user.first_name}</td>
+                <td className="align-middle">{user.last_name}</td>
+                <td className="align-middle">{user.email}</td>
+                <td className="align-middle">{user.login_by === 0 ? "Facebook" : "Google"}</td>
             </tr>
         )
     }
@@ -116,34 +66,14 @@ class Users extends Component {
                                 <h4 className="card-header-custom">Users</h4>
                             </CardHeader>
                             <CardBody>
-                                <Row className="align-items-right">
-                                    <Col sm="12" md="6" xl="4" className="mb-3 mb-xl-0">
-                                    </Col>
-                                    <Col sm="12" md="6" xl="8" className="mb-3 mb-xl-0">
-                                        <Row className="justify-content-end">
-                                            <Col sm="12" md="6" xs xl="5" className="mb-3 mb-xl-0">
-                                                <Input type="text" placeholder={`Search by name`}
-                                                    value={this.state.search_keyword}
-                                                    name="search_keyword" onChange={(event) => this.changeSearch(event)}
-                                                    onKeyPress={(event) => this.enterPressed(event)} />
-                                            </Col>
-                                            <Col sm="12" md="4" xs xl="3" className="mb-3 mb-xl-0">
-                                                <Button block className="black-btn" color="secondary" size="sm" onClick={() => this.search()}>Search</Button>
-                                            </Col>
-                                        </Row>
-                                    </Col>
-                                </Row>
-                            </CardBody>
-                            <CardBody>
                                 <Table bordered striped responsive size="sm">
                                     <thead>
                                         <tr>
                                             <th scope="col" className="text-center">No</th>
-                                            <th scope="col">Image</th>
-                                            <th scope="col">Name</th>
+                                            <th scope="col">First Name</th>
+                                            <th scope="col">Last Name</th>
                                             <th scope="col">Email</th>
-                                            <th scope="col">Address</th>
-                                            <th scope="col" className="text-center">Actions</th>
+                                            <th scope="col">Logged In By</th>
                                         </tr>
                                     </thead>
                                     {this.state.users.length > 0 ?
@@ -185,8 +115,7 @@ class Users extends Component {
 
 const mapStateToProps = state => {
     return {
-        global_page_no: state.reducer.page_no,
-        global_search_keyword: state.reducer.search_keyword
+        global_page_no: state.reducer.page_no
     }
 }
 
